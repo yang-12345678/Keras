5b44751067048482d6462b04cd2da2c6c45f5eae
yang-12345678 <1958523752@qq.com>
2021-05-08 15:56:52 +0800

    。

--- image_rename.py
+++ image_rename.py
@@ -1,13 +1,13 @@
 import os
 
-path = 'C:\\Users\\yang\\Desktop\\nongzuowu\\datasets'
+path = 'C:\\Users\\yang\\Desktop'
 
-base_dir = os.path.join(path, 'weakness')
+base_dir = os.path.join(path, '222')
 # print(os.listdir(西瓜))
 
 for i, name in enumerate(os.listdir(base_dir)):
     fname = os.path.join(base_dir, name)
     # print(fname)
-    new = os.path.join(base_dir, 'weakness' + '.' + f'{i}' + '.jpg')
+    new = os.path.join(base_dir, 'weakness_l' + '.' + f'{i}' + '.jpg')
 
     os.rename(fname, new)
