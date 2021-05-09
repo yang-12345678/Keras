ea8fb0807684489affca0660e3b34b2e826d045a
yang-12345678 <1958523752@qq.com>
2021-05-09 12:35:54 +0800

    2021.5.9

--- image_rename.py
+++ image_rename.py
@@ -2,12 +2,12 @@ import os
 
 path = 'C:\\Users\\yang\\Desktop'
 
-base_dir = os.path.join(path, '222')
+base_dir = os.path.join(path, '444')
 # print(os.listdir(西瓜))
 
 for i, name in enumerate(os.listdir(base_dir)):
     fname = os.path.join(base_dir, name)
     # print(fname)
-    new = os.path.join(base_dir, 'weakness_l' + '.' + f'{i}' + '.jpg')
+    new = os.path.join(base_dir, 'weakness_ll' + '.' + f'{i}' + '.jpg')
 
     os.rename(fname, new)
---
+++ 互联网+3.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import keras
+
+from keras.applications import VGG16
+
+conv_base = VGG16(weights='imagenet',
+                  include_top=False,
+                  input_shape=(150, 150, 3))
+
+import os
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+
+base_dir = "C:/Users/yang/Desktop/nongzuowu/small/"
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+datagen = ImageDataGenerator(rescale=1. / 255)
+batch_size = 20
+
+
+def extract_features(directory, sample_count):
+    features = np.zeros(shape=(sample_count, 4, 4, 512))
+    labels = np.zeros(shape=(sample_count))
+    generator = datagen.flow_from_directory(
+        directory,
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='binary')
+
+    i = 0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size:(i + 1) * batch_size] = features_batch
+        labels[i * batch_size:(i + 1) * batch_size] = labels_batch
+        i += 1
+        if i * batch_size >= sample_count:
+            break
+    return features, labels
+
+
+train_features, train_labels = extract_features(train_dir, 2540)
+validation_features, validation_labels = extract_features(validation_dir, 1000)
+test_features, test_labels = extract_features(test_dir, 1000)
+
+train_features = np.reshape(train_features, (2540, 4 * 4 * 512))
+validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
+test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
+
+from keras import models
+from keras import layers
+from keras import regularizers
+
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(256, kernel_regularizer=regularizers.l2(0.001),
+                       activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.summary()
+
+conv_base.trainable = True
+
+set_trainable = False
+for layer in conv_base.layers:
+    if layer.name == 'block5_conv1':
+        set_trainable = True
+    if set_trainable:
+        layer.trainable = True
+    else:
+        layer.trainable = False
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    horizontal_flip=0.2,
+    fill_mode='nearest')
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+validation_genetator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-5),
+              metrics=['acc'])
+
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=127,
+                              epochs=100,
+                              validation_data=validation_genetator,
+                              validation_steps=50,
+                              )
+
+model.save("hulianwang+(sample=2540).h5")
+
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+test_loss, test_acc = model.evaluate_generator(test_generator, steps=50)
+
+print(test_acc)
